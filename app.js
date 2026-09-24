a06916e50a772b62d3cbc2aa2f6b9e85f61e118a
jorew <jorewmario@gmail.com>
2026-09-24 14:09:46 -0300

    app.js-conserto

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 */
 
-var createError = require('http-errors');
+/* var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -103,6 +103,77 @@ app.use(function(req, res, next) {
 });
 
 // Trata outros erros
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+*/
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// 1. Importa o driver nativo do MongoDB
+const { MongoClient } = require('mongodb');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var contextRouter = require('./routes/contexto');
+var armRouter = require('./routes/armaduras');
+
+var app = express();
+
+// 2. String de Conexão com o MongoDB Atlas
+// ATENÇÃO: Substitua <sua_senha> pela sua senha real do banco de dados
+const uri = "mongodb+srv://jorewmario_db_user:<sua_senha>@testandomongodb.izcp8zd.mongodb.net/exercicos?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+// 3. Função assíncrona para garantir a conexão ANTES de receber requisições
+async function conectarBanco() {
+  try {
+    await client.connect();
+    console.log('✅ Conectado com sucesso ao MongoDB Atlas!');
+    
+    // Guarda a referência do banco de dados 'exercicos' na aplicação Express
+    app.set('db', client.db('exercicos'));
+  } catch (err) {
+    console.error('❌ Erro CRÍTICO ao conectar ao MongoDB Atlas:', err);
+  }
+}
+
+// Inicia a tentativa de conexão
+conectarBanco();
+
+// Configuração da View Engine (EJS)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
+// Configuração das Rotas
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/contexto', contextRouter);
+app.use('/armaduras', armRouter);
+
+// Tratamento de erro 404
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Tratamento de erros do servidor
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
